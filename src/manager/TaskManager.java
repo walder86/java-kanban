ec4fb26db516ba04fd8c1e00b41ec68fbe673780
vladimir.vasiliev <walder86@yandex.ru>
2025-02-12 19:58:47 +0300

    main. В менеджер задач добавлены методы по получению списка задач и добавлению для всех типов задач.

--- src/manager/TaskManager.java
+++ src/manager/TaskManager.java
@@ -4,12 +4,58 @@ import typeTasks.Epic;
 import typeTasks.SubTask;
 import typeTasks.Task;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class TaskManager {
-    private HashMap<Integer, Epic> epics;
-    private HashMap<Integer, Task> tasks;
-    private HashMap<Integer, SubTask> subTasks;
 
+    public static Integer countTasks = 1;
 
+    private Map<Integer, Epic> epics;
+    private Map<Integer, Task> tasks;
+    private Map<Integer, SubTask> subTasks;
+
+    public TaskManager() {
+        epics = new HashMap<>();
+        tasks = new HashMap<>();
+        subTasks = new HashMap<>();
+    }
+
+    public List<Epic> getAllEpics() {
+        return new ArrayList<>(epics.values());
+    }
+
+    public List<Task> getAllTasks() {
+        return new ArrayList<>(tasks.values());
+    }
+
+    public List<SubTask> getAllSubTasksForEpic(Integer epicId) {
+        return epics.get(epicId).getSubTasks();
+    }
+
+    public boolean addTask(Task task) {
+        tasks.put(task.getId(), task);
+        System.out.println("Задача успешно добавлена");
+        return true;
+    }
+
+    public boolean addEpic(Epic epic) {
+        tasks.put(epic.getId(), epic);
+        System.out.println("Эпик успешно добавлен");
+        return true;
+    }
+
+    public boolean addSubTask(SubTask subTask, Integer epicId) {
+        Epic epic = epics.get(epicId);
+        if (epic != null) {
+            epic.getSubTasks().add(subTask);
+            System.out.println("Подзадача успешно добавлена");
+            return true;
+        } else {
+            System.out.println("Эпик не найден");
+            return false;
+        }
+    }
 }
